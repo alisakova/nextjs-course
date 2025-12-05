cb5a4e234c650d5094fd0b4045d428eac86e73d8
alisakova <a.lisakova@morbo.ru>
2025-12-05 12:46:57 +0300

    Добавлен classnames

--- src/app/_components/NavLink/NavLink.tsx
+++ src/app/_components/NavLink/NavLink.tsx
@@ -3,6 +3,7 @@
 import styles from './navLink.module.css'
 import Link, { LinkProps } from 'next/link'
 import { usePathname } from 'next/navigation'
+import classNames from "classnames";
 
 interface NavLinkProps extends Pick<LinkProps, 'href'> {
   children: string
@@ -12,7 +13,9 @@ export const NavLink = ({ children, href }: NavLinkProps) => {
   const pathname = usePathname()
 
   return (
-    <Link href={href} className={`${styles.link} ${pathname === href ? styles.isActive : ''}`}>
+    <Link href={href} className={classNames(styles.link, {
+      [styles.isActive]: pathname === href
+    })}>
       {children}
     </Link>
   )
--- src/app/_components/RacketImage/RacketImage.tsx
+++ src/app/_components/RacketImage/RacketImage.tsx
@@ -1,9 +1,10 @@
 import styles from './racketImage.module.css'
 import Image, { ImageProps } from 'next/image'
+import classNames from "classnames";
 
 export const RacketImage = ({ src, alt, className }: ImageProps) => {
   return (
-    <div className={`${styles.racketImage} ${className ? className : ''}`}>
+    <div className={classNames(styles.racketImage, className)}>
       <Image objectFit="contain" className={styles.image} fill src={src} alt={alt} />
     </div>
   )
