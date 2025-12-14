3f903712365aa810812b07910914d6ea925f0b66
alisakova <a.lisakova@morbo.ru>
2025-12-14 13:29:32 +0300

    Созданы компоненты Header, Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,54 +1,23 @@
 import './global.css'
 
 import styles from './layout.module.css'
-import { NavLink } from './components/NavLink'
 import { Metadata } from 'next'
-import Link from 'next/link'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer/Footer'
 
 export const metadata: Metadata = {
   title: 'Tennis store',
 }
 
-const navLinks = [
-  {
-    title: 'Главная',
-    href: '/',
-  },
-  {
-    title: 'Ракетки',
-    href: '/rackets',
-  },
-  {
-    title: 'Топ 10',
-    href: '/top-10',
-  },
-] as const
-
 const RootLayout = ({ children }: LayoutProps<'/'>) => {
   return (
     <html>
       <body>
-        <header className={styles.header}>
-          <Link href="/" className={styles.appName}>
-            Tennis store
-          </Link>
-          <nav className={styles.nav}>
-            <ul className={styles.navList}>
-              {navLinks.map(({ title, href }) => (
-                <NavLink key={href} href={href}>
-                  {title}
-                </NavLink>
-              ))}
-            </ul>
-          </nav>
-        </header>
+        <Header />
 
         <main className={styles.main}>{children}</main>
 
-        <footer className={styles.footer}>
-          <p className={styles.appName}>Tennis store</p>
-          <p className={styles.copyRightText}>&copy; 2025 Tennis Store. All rights reserved.</p>
-        </footer>
+        <Footer />
       </body>
     </html>
   )
